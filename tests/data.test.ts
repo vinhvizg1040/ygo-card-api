7f798d0ac9a90a4944e812f5bae187d2e07b0e4e
Thái Vĩnh <vinhvizg1040@gmail.com>
2024-07-20 16:16:15 +0700

    cm

---
+++ tests/data.test.ts
@@ -0,0 +1,18 @@
+import { Data } from "../index";
+const path = require('path');
+
+describe("Pagination function", () => {
+  let data: Data;
+  // const jsonFilePath = path.join(__dirname, '../cards.json');
+  beforeAll(async () => {
+    // Load test data from JSON file before tests
+    // data = new Data(jsonFilePath);
+    // Or from api
+    data = new Data();
+  });
+  it("should check non-empty data", async () => {
+    const result = await data.getData();
+    // Check if the array is not empty
+    expect(result.length).toBeGreaterThan(0);
+  });
+});
